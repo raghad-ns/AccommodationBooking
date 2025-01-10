2e6d758ea88d9f1765e50c6a6660999bf8a63431
Raghad Salem <raghadnadersalem20022017@gmail.com>
2025-01-10 21:27:12 +0200

    Bug fix

--- AccommodationBooking.Domain/Rooms/Services/RoomService.cs
+++ AccommodationBooking.Domain/Rooms/Services/RoomService.cs
@@ -13,31 +13,31 @@ public class RoomService : IRoomService
     }
     async Task<Room> IRoomService.AddOne(Room room)
     {
-        return await _roomRepository.CreateRoom(room);
+        return await _roomRepository.AddOne(room);
     }
 
     async Task IRoomService.DeleteOne(int roomId)
     {
-        await _roomRepository.DeleteRoomById(roomId);
+        await _roomRepository.DeleteOne(roomId);
     }
 
     async Task<Room> IRoomService.GetOne(int id)
     {
-        return await _roomRepository.GetRoomById(id);
+        return await _roomRepository.GetOne(id);
     }
 
     async Task<Room> IRoomService.GetOneByNumber(string number)
     {
-        return await _roomRepository.GetRoomByNumber(number);
+        return await _roomRepository.GetOneByNumber(number);
     }
 
     async Task<List<Room>> IRoomService.Search(int page, int pageSize, RoomFilters roomFilters)
     {
-        return await _roomRepository.GetRooms(page, pageSize, roomFilters);
+        return await _roomRepository.Search(page, pageSize, roomFilters);
     }
 
     async Task<Room> IRoomService.UpdateOne(int roomId, Room room)
     {
-        return await _roomRepository.UpdateRoom(roomId, room);
+        return await _roomRepository.UpdateOne(roomId, room);
     }
 }
